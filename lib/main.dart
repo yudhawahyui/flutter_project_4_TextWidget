273fd38ed21c216c04f6f85acb69b5ce87beff8e
Your Name <yudhaxsinatra@gmail.com>
2022-11-18 10:56:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Tutorial",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Text Widget Tutorial"),
+        ),
+        body: const Center(
+          child: Text("Hello World! This is a text widget"),
+        ),
+      ),
+    );
+  }
+}
